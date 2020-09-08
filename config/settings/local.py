8c4b1fd7e0438a806e201e71748533beab0e6ef9
enversel <54875204+enversel@users.noreply.github.com>
2020-09-09 00:00:48 +0900

    settings분리

---
+++ config/settings/local.py
@@ -0,0 +1,4 @@
+from .base import *
+# python manage.py runserver --settings=config.settings.local
+
+ALLOWED_HOSTS = []
---
+++ config/settings/prod.py
@@ -0,0 +1,4 @@
+from .base import *
+# python manage.py runserver --settings=config.settings.prod
+
+ALLOWED_HOSTS = ['3.34.22.53']
