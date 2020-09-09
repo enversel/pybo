a65e6083a368cd99cbe5d24d041ea008be01ad60
enversel <54875204+enversel@users.noreply.github.com>
2020-09-09 23:40:59 +0900

    STATIC_ROOT 생성

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -2,3 +2,5 @@ from .base import *
 # python manage.py runserver --settings=config.settings.prod
 
 ALLOWED_HOSTS = ['3.34.22.53']
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = []
